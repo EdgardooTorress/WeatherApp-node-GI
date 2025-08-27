6dde9f15a20b084048cc9c0b16004d8a2b9ce37e
Edgardo Torres <ejtf99usa@gmail.com>
2025-08-27 19:54:04 -0400

    DONE

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,23 +2,23 @@
 console.log("cliente side javascript file is working")
 
 
-
+// Get elements from the DOM
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 
-
+// create an even to handle the submit
 weatherForm.addEventListener('submit', (e)=> {
     e.preventDefault()
 
-    const location = search.value
+    const location = search.value // get the calue from the imput
 
-    messageOne.textContent = "Loading ..."
+    messageOne.textContent = "Loading ..."  // display loading while getting the data from API
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => { 
     response.json().then((data) => {
         if(data.error){
            messageOne.textContent = data.error
